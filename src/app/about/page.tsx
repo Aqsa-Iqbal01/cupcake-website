eaf39e5c5e29a2b0364d7c1d00904d6b529f5ef8
Aqsa Iqbal <164233800+Aqsa-Iqbal01@users.noreply.github.com>
2024-11-10 22:51:03 +0500

    updated index.html

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,7 +28,7 @@ const About = () => {
                         Our Story: Smallcakes Fort Myers is owned and operated by Don and Tonyia Moyer, a local couple, and is dedicated to bringing you the most memorable cupcake and creamery experience. We are located in the beautiful Gulf Coast town of Fort Myers and have been serving the community since 2013.
                     </p>
                     <p>
-                        Featured on TV: Smallcakes has proven to be a nationwide success. Our cupcake bakery has been featured for three seasons running on the Food Network&apos;s "CUPCAKE WARS", an appearance on ABC'S hit daytime talk show, "THE VIEW", and was selected as a "Must Try Cupcake Shop" by USA TODAY. We are honored to have the opportunity to bring gourmet cupcakes to the Southwest Florida community.
+                        Featured on TV: Smallcakes has proven to be a nationwide success. Our cupcake bakery has been featured for three seasons running on the Food Network&apos;s &quot;CUPCAKE WARS&quot;, an appearance on ABC&apos;S hit daytime talk show, &quot;THE VIEW&quot;, and was selected as a &quot;Must Try Cupcake Shop&quot; by USA TODAY. We are honored to have the opportunity to bring gourmet cupcakes to the Southwest Florida community.
                     </p>
                     <p>
                         Made Fresh, With Care: We make our gourmet cupcakes on-site every day using only the highest quality ingredients, and we sweeten them with love! We also provide personalized cakes for weddings, birthdays, baby showers, business gatherings, and any other special occasion. With a delectable cake that looks as good as it tastes, we aim to make your event one to remember.
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,14 @@
-import Image from "next/image";
 import Navbar from "./components/Navbar";
-import "@/app/styles/home.css"
+import "@/app/styles/home.css";
+
 export default function Home() {
   return (
-    
     <div className="home">
-       <Navbar/>
-     
-      
-    <div className="heading">
-            <h1 >Welcome to Cakes Galore</h1>
-            <p className="para">Your one-stop shop for delicious cakes!</p>
-            </div>
-        
-     
+      <Navbar />
+      <div className="heading">
+        <h1>Welcome to Cakes Galore</h1>
+        <p className="para">Your one-stop shop for delicious cakes!</p>
+      </div>
     </div>
-  )
+  );
 }
